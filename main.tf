aa1302204f88402c757839d63ac0b4ac59357eaa
Bryan Recinos <bryan.rhm1@gmail.com>
2022-06-17 23:18:24 -0600

    [v1.0.0] initial module implementation

---
+++ main.tf
@@ -0,0 +1,40 @@
+resource "aws_iam_openid_connect_provider" "oidc" {
+  url             = var.github_url
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.certificate.certificates.0.sha1_fingerprint]
+}
+
+data "tls_certificate" "certificate" {
+  url = var.github_url
+}
+
+resource "aws_iam_role" "role" {
+  assume_role_policy    = data.aws_iam_policy_document.asume_role_policy.json
+  name                  = var.role_name
+  managed_policy_arns   = var.managed_policy_arns
+  force_detach_policies = true 
+}
+
+data "aws_iam_policy_document" "asume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    condition {
+      test     = "StringLike"
+      variable = "${replace(aws_iam_openid_connect_provider.oidc.url, "https://", "")}:sub"
+      values   = [ for repo in var.github_repositories : "repo:${var.github_organization}/${repo}:*" ]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(aws_iam_openid_connect_provider.oidc.url, "https://", "")}:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+    principals {
+      identifiers = [aws_iam_openid_connect_provider.oidc.arn]
+      type        = "Federated"
+    }
+  }
+}
+
